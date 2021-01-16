3c54fb762fbb2ddfb92da6ed0ef1e348eb50db87
mrmamedzade <cafarmm@code.edu.az>
2021-01-16 19:24:58 +0400

    Extasks

---
+++ Stringtask1/Stringtask1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        public static StringBuilder StringBuilder { get; private set; }
+
+        static void Main(string[] args)
+        {
+        Bashlangic:
+            Console.WriteLine("Birinci Reqemi Daxil Edin(3 reqemli eded):");
+            string number1 = Console.ReadLine().Trim();
+            Console.WriteLine("Ikinci Reqemi Daxil Edin(5 reqemli eded):");
+            string number2 = Console.ReadLine().Trim();
+
+            if (number1.Length < 3 || number2.Length < 5)
+            {
+                Console.WriteLine("Zehmet olmasa birinci reqem yerine 3 ikinciye ise 5 reqem daxil edin.");
+                goto Bashlangic;
+            }
+            else
+            {
+                number1 = number1.Substring(0, 2);
+                number2 = number2.Substring(number2.Length - 4, 4);
+                char[] c = number2.ToCharArray();
+                StringBuilder s = new StringBuilder(number1);
+
+                foreach (char number in c)
+                {
+                    s.Append(number);
+                }
+                int new_number = Int32.Parse(s.ToString());
+
+                int last_numbers = new_number % 1000;
+                double first_numbers = new_number / 1000;
+                int first_numbers_vol2 = Convert.ToInt32(first_numbers);
+
+                int result = last_numbers - first_numbers_vol2;
+
+                if (result < 0)
+                {
+                    Console.WriteLine("Netice: " + result);
+                    Console.WriteLine("Netice Menfi eded alindi,yeniden yazmaginiz xahish olunur...(Musbet eded yazilmalidir)");
+                    goto Bashlangic;
+                }
+                else
+                {
+                    Console.WriteLine("Netice: " + result);
+                }
+            }
+        }
+    }
+}
---
+++ Stringtask2.1/Stringtask2.1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Evvel:
+            Console.WriteLine("Bir Soz daxil edin:");
+            string word = Console.ReadLine().Trim().ToLower();
+            string letter_a = "a";
+            string letter_b = "b";
+            int result1 = word.IndexOf(letter_a, 0, word.Length - 1);
+            int result2 = word.IndexOf(letter_b, 0, word.Length - 1);
+
+            if (word.Length != 6)
+            {
+                Console.WriteLine("Daxil etdiyiniz soz 5 herfden ve sonunda noqteden ibaret olsun...");
+                goto Evvel;
+            }
+            else
+            {
+                if (result1 != -1)
+                {
+                    Console.WriteLine("Daxil etdiyiniz sozde a herfi olsun...");
+                    goto Evvel;
+                }
+                else
+                {
+                    if (result1 != -1)
+                    {
+                        Console.WriteLine("Daxil etdiyiniz sozde a herfi olmasin...");
+                        goto Evvel;
+                    }
+                    else
+                    {
+                        if (result2 == -1)
+                        {
+                            Console.WriteLine("Daxil etdiyiniz sozde en azi bir b herfi olsun...");
+                            goto Evvel;
+                        }
+                        else
+                        {
+                            if (word.EndsWith('.'))
+                            {
+                                 
+
+                                string word1 = word.Substring(0, 2);
+                                string word2 = word.Substring(word.Length - 4, 3);
+
+                                char[] ch = word1.ToCharArray();
+                                StringBuilder sb = new StringBuilder(word2);
+                                foreach (char new_word in ch)
+                                {
+                                    sb.Append(new_word);
+                                }
+                                Console.WriteLine(sb.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Daxil etdiyiniz soz 5 herfden ve sonda noqteden ibaret olsun...");
+                                goto Evvel;
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
